7d621db3618db90679461550fb0c952417616402
Michael Chau <mchau95@gmail.com>
2016-01-12 12:56:48 -0500

    Add Bot.run and an on_ready message

--- bot.py
+++ bot.py
@@ -17,5 +17,15 @@ class Bot(commands.Bot):
 		self.twitch_ID = config['TWITCH']['client_id']
 		self.carbon_key = config['CARBON']['key']
 
+	async def on_ready(self):
+		print("Logged in as {}".format(self.user.name))
+		print("User ID: {}".format(self.user.id))
+		print("Library: {} - {}".format(discord.__title__, discord.__version__))
+
 	def run(self):
-		pass
+		try:
+			self.loop.run_until_complete(self.start(self.email, self.password))
+		except KeyboardInterrupt:
+			self.loop.run_until_complete(self.logout())
+		finally:
+			self.loop.close()
