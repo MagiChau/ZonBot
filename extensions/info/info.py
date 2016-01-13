5d02f415aa972b901138c36f97bca025f0cfc7cf
MagiChau <mchau95@gmail.com>
2016-01-13 00:47:08 -0500

    add info server command

--- extensions/info/info.py
+++ extensions/info/info.py
@@ -16,9 +16,19 @@ class Info():
         if ctx.message.channel.is_private:
             msg = "```Channel ID: {}```".format(ctx.message.channel.id)
         else:
-            msg = "```Channel Name: {}\nChannel ID: {}".format(ctx.message.channel.name, ctx.message.channel.id)
-            msg += "\nServer Name: {}\nServer ID: {}```".format(ctx.message.server.name, ctx.message.server.id)
+            msg = "```Channel: {}\nChannel ID: {}".format(ctx.message.channel.name, ctx.message.channel.id)
+            msg += "\nServer: {}\nServer ID: {}```".format(ctx.message.server.name, ctx.message.server.id)
         await self.bot.say(msg)
+
+    @info.command(name='server', pass_context=True)
+    async def _server(self, ctx):
+        try:
+            s = ctx.message.server
+            msg = "Server: {}\nID: {}\nOwner: {}\nRegion: {}\nMembers: {}\nIcon: {}".format(s.name, s.id,
+                s.owner.name, s.region, str(len(s.members)), s.icon_url)
+            await self.bot.say(msg)
+        except AttributeError:
+            await self.bot.say("Command cannot be used in private channels.")
 
 def setup(bot):
     bot.add_cog(Info(bot))
