04de77289d4182796d26c39523fc67b7876fe876
MagiChau <mchau95@gmail.com>
2016-01-13 23:52:39 -0500

    add info user command
    Only searches the server command is used in. If used in a private
    channel can only find the message author.

--- extensions/info/info.py
+++ extensions/info/info.py
@@ -32,8 +32,42 @@ class Info():
 
     @info.command(name='user', pass_context=True)
     async def _user(self, ctx, user : str):
-        pass
+        msg = "No user found."
+        found = None
+        if re.fullmatch(r'<@[0-9]+>', user):
+            if ctx.message.server is None:
+                found = ctx.message.author if ctx.message.author.mention == user else None
+            else:
+                found = discord.utils.get(ctx.message.server.members, mention=user)
+        elif re.fullmatch(r'[0-9]+', user):
+            if ctx.message.server is None:
+                found = ctx.message.author if ctx.message.author.id == user else None
+            else:
+                found = discord.utils.get(ctx.message.server.members, id=user)
+        else:
+            if ctx.message.server is None:
+                found = ctx.message.author if user.lower() in ctx.message.author.name.lower() else None
+            else:
+                found = discord.utils.find(lambda m: user.lower() in m.name.lower(), ctx.message.server.members)
 
+        if found:
+            msg = "```User: {}\nID: {}\nAvatar: {}".format(found.name, found.id, found.avatar_url)
+            if not hasattr(found, 'server'):
+                msg += "```"
+            else:
+                roles_str = str()
+                for role in found.roles:
+                    role_name = role.name
+                    if role_name.startswith('@'): 
+                        role_name = role_name.replace('@', '@\u200B')
+                    roles_str += role_name + ', '
+                roles_str = roles_str[:-2] if roles_str[-2:] == ', ' else roles_str
+                msg += "\nRoles: {}\nServer Join Date: {}/{}/{}```".format(roles_str, found.joined_at.month,
+                    found.joined_at.day, found.joined_at.year)
+        elif ctx.message.server is None:
+            msg += " You can only use the command on yourself in a private channel."
+
+        await self.bot.say(msg)
 
 def setup(bot):
     bot.add_cog(Info(bot))
