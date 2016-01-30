f059d5c65dae71f078ead2ccf084455e11890f4e
MagiChau <mchau95@gmail.com>
2016-01-29 21:06:27 -0500

    reformat info help

--- extensions/info/info.py
+++ extensions/info/info.py
@@ -5,24 +5,40 @@ class Info():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True, help="Info about the bot. You can also use the following subcommands")
+    @commands.group(pass_context=True)
     async def info(self, ctx):
+        """Displays various information
+
+        Bot Info
+        Usage: !info
+
+        Invoke Subcommand
+        Usage: !info <command>
+        """
+
         if ctx.invoked_subcommand is None:
             msg = "Bot created by ZonMachi.\nDeveloped using the discord.py library."
             msg += "\nCurrently connected to {} servers.".format(str(len(self.bot.servers)))
             await self.bot.say(msg)
 
-    @info.command(name='channel', pass_context=True, help="Lists information about a channel.")
+    @info.command(name='channel', pass_context=True)
     async def _channel(self, ctx):
+        """Displays information about channel command is invoked in"""
+
         if ctx.message.channel.is_private:
             msg = "```Channel ID: {}```".format(ctx.message.channel.id)
         else:
             msg = "```Channel: {}\nChannel ID: {}".format(ctx.message.channel.name, ctx.message.channel.id)
-            msg += "\nServer: {}\nServer ID: {}```".format(ctx.message.server.name, ctx.message.server.id)
+            msg += "\nServer: {}\nServer ID: {}".format(ctx.message.server.name, ctx.message.server.id)
+            if ctx.message.channel.topic:
+                msg +="\nTopic: {}".format(ctx.message.channel.topic)
+            msg += "```"
         await self.bot.say(msg)
 
-    @info.command(name='server', pass_context=True, help = "Lists information about a server.")
+    @info.command(name='server', pass_context=True)
     async def _server(self, ctx):
+        """Displays information about server command is invoked in"""
+
         try:
             s = ctx.message.server
             msg = "Server: {}\nID: {}\nOwner: {}\nRegion: {}\nMembers: {}\nIcon: {}".format(s.name, s.id,
@@ -31,25 +47,32 @@ class Info():
         except AttributeError:
             await self.bot.say("Command cannot be used in private channels.")
 
-    @info.command(name='user', pass_context=True, help="Lists information about a user.")
-    async def _user(self, ctx, user : str):
+    @info.command(name='user', pass_context=True)
+    async def _user(self, ctx, name : str):
+        """Displays information about a user in the server command is invoked in
+
+        Usage: !info user <name>
+        name: Can be part of a user name, a mention, or a user ID
+        If multiple people have the same user name use a mention or user ID
+        """
+
         msg = "No user found."
         found = None
-        if re.fullmatch(r'<@[0-9]+>', user):
+        if re.fullmatch(r'<@[0-9]+>', name):
             if ctx.message.server is None:
-                found = ctx.message.author if ctx.message.author.mention == user else None
+                found = ctx.message.author if ctx.message.author.mention == name else None
             else:
-                found = discord.utils.get(ctx.message.server.members, mention=user)
-        elif re.fullmatch(r'[0-9]+', user):
+                found = discord.utils.get(ctx.message.server.members, mention=name)
+        elif re.fullmatch(r'[0-9]+', name):
             if ctx.message.server is None:
-                found = ctx.message.author if ctx.message.author.id == user else None
+                found = ctx.message.author if ctx.message.author.id == name else None
             else:
-                found = discord.utils.get(ctx.message.server.members, id=user)
+                found = discord.utils.get(ctx.message.server.members, id=name)
         else:
             if ctx.message.server is None:
-                found = ctx.message.author if user.lower() in ctx.message.author.name.lower() else None
+                found = ctx.message.author if name.lower() in ctx.message.author.name.lower() else None
             else:
-                found = discord.utils.find(lambda m: user.lower() in m.name.lower(), ctx.message.server.members)
+                found = discord.utils.find(lambda m: name.lower() in m.name.lower(), ctx.message.server.members)
 
         if found:
             msg = "```User: {}\nID: {}\nAvatar: {}".format(found.name, found.id, found.avatar_url)
@@ -70,8 +93,10 @@ class Info():
 
         await self.bot.say(msg)
 
-    @commands.command(help="States bot total running time.")
+    @commands.command()
     async def uptime(self):
+        """Displays bot's total running time"""
+
         seconds = int(time.time() - self.bot.start_time)
         minutes = seconds // 60
         seconds -= minutes * 60
