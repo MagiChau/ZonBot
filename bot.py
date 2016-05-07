0443dd099c0898a6d5155c9107e0b4184ac6295e
MagiChau <mchau95@gmail.com>
2016-05-07 16:35:01 -0400

    Remove reconnect logic
    WS changes in discord.py deprecated reconnect logic

--- bot.py
+++ bot.py
@@ -14,22 +14,6 @@ class Bot(commands.Bot):
 		self._initialize_extensions()
 		self.start_time = 0
 
-	async def sane_connect(self):
-		self.gateway = await self._get_gateway()
-		await self._make_websocket()
-
-		while not self.is_closed:
-			msg = await self.ws.recv()
-			if msg is None:
-				if self.ws.close_code == 1012:
-					await self.redirect_websocket(self.gateway)
-					continue
-				else:
-					# Connection was dropped, break out
-					break
-
-			await self.received_message(msg)
-
 	def _load_config_data(self):
 		self.email = config.email
 		self.password = config.password
@@ -66,23 +50,5 @@ class Bot(commands.Bot):
 			await self.process_commands(message)
 		except Exception as e:
 			print("Command Error Caught At Top Level")
+			print(message.content)
 			print(e)
-
-	def run(self):
-		policy = asyncio.get_event_loop_policy()
-		policy.set_event_loop(self.loop)
-		#self.loop.set_debug(True)
-		while True:
-			try:
-				self.loop.run_until_complete(self.login(self.token))
-			except (discord.HTTPException, aiohttp.ClientError):
-				print("Failed to login to Discord")
-				self.loop.run_until_complete(asyncio.sleep(15))
-			else:
-				break #if no error break out of the while loop
-		while not self.is_closed:
-			try:
-				self.loop.run_until_complete(self.sane_connect())
-			except (discord.HTTPException, aiohttp.ClientError):
-				print("Lost Connection")
-				self.loop.run_until_complete(asyncio.sleep(15))
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import bot
 
 if __name__ == '__main__':
 	zonbot = bot.Bot('!', pm_help = True)
-	zonbot.run()
+	zonbot.run(zonbot.token)
