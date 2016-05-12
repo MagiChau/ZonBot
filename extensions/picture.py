a6b034ab330f9159f3ca15800477c74b577bab3e
MagiChau <mchau95@gmail.com>
2016-05-11 20:07:21 -0400

    add ohayou pictures and refactor pictures cog

--- extensions/picture.py
+++ extensions/picture.py
@@ -1,58 +1,55 @@
 from discord.ext import commands
 import discord
 import os
+import random
 
 class Picture():
 
     def __init__(self, bot):
         self.bot = bot
+        self.picture_path = os.path.join(os.path.dirname(os.path.realpath(__file__)) + "/picture/")
+
+    async def upload_picture(self, name):
+        dp = os.path.join(self.picture_path + "/{0}/".format(name))
+        files = os.listdir(dp)
+        if len(files) == 0:
+            return
+        fp = os.path.join(dp + files[random.randint(0, len(files) - 1)])
+        with open(fp, 'rb') as f:
+            try:
+                await self.bot.upload(f)
+            except discord.Forbidden:
+                pass
 
     @commands.command()
     async def awoo(self):
         """awoo"""
 
-        fp = os.path.join(os.path.dirname(os.path.realpath(__file__)) + '/picture/awoo.jpg')
-        with open(fp, 'rb') as f:
-            try:
-                await self.bot.upload(f)
-            except discord.Forbidden:
-                pass
-                #await self.bot.say("Error: bot does not have permission to upload pictures.")
+        await self.upload_picture("awoo")
 
     @commands.command()
     async def baka(self):
-        """Baka baka"""
+        """baka baka"""
 
-        fp = os.path.join(os.path.dirname(os.path.realpath(__file__)) + '/picture/baka.jpg')
-        with open(fp, 'rb') as f:
-            try:
-                await self.bot.upload(f)
-            except discord.Forbidden:
-                pass
-                #await self.bot.say("Error: bot does not have permission to upload pictures.")
+        await self.upload_picture("baka")
 
     @commands.command()
     async def nobully(self):
         """No bullying allowed"""
 
-        fp = os.path.join(os.path.dirname(os.path.realpath(__file__)) + '/picture/nobully.jpg')
-        with open(fp, 'rb') as f:
-            try:
-                await self.bot.upload(f)
-            except discord.Forbidden:
-                pass
-                #await self.bot.say("Error: bot does not have permission to upload pictures.")
+        await self.upload_picture("nobully")
+
+    @commands.command()
+    async def ohayou(self):
+        """ohayou"""
+
+        await self.upload_picture("ohayou")
 
     @commands.command()
     async def sad(self):
         """Sad things are sad"""
-        fp = os.path.join(os.path.dirname(os.path.realpath(__file__)) + '/picture/sad.jpg')
-        with open(fp, 'rb') as f:
-            try:
-                await self.bot.upload(f)
-            except discord.Forbidden:
-                pass
-                #await self.bot.say("Error: bot does not have permission to upload pictures.")
+
+        await self.upload_picture("sad")
 
 def setup(bot):
     bot.add_cog(Picture(bot))
