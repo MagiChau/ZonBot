d3f0d24925018bf19e7907366bc142ce56ac2302
MagiChau <mchau95@gmail.com>
2016-06-04 22:22:24 -0400

    Refactor prune to use mass delete API call
    Prune now uses display_name rather than usernames

--- extensions/moderate.py
+++ extensions/moderate.py
@@ -1,3 +1,4 @@
+import asyncio
 import checks
 from discord.ext import commands
 import discord
@@ -7,7 +8,7 @@ class Moderate():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, hidden=True)
     async def prunebot(self, ctx, num : int):
         """Deletes ZonBot's recent messages
 
@@ -32,11 +33,11 @@ class Moderate():
 
             if not del_flag: break
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, hidden=True)
     @checks.is_not_pvt_chan()
     @checks.can_manage_message()
     async def prune(self, ctx, name : str, amount : int=10):
-        """Deletes messages of a specified user
+        """Deletes messages of a specified user.
         By default 10 messages from recent history will be deleted.
 
         Usage: !prune <user> [amount]
@@ -45,7 +46,7 @@ class Moderate():
         """
 
         try:
-            found = discord.utils.find(lambda m: name.lower() in m.name.lower(), ctx.message.server.members)
+            found = discord.utils.find(lambda m: name.lower() in m.display_name.lower(), ctx.message.server.members)
         except:
             found = None
 
@@ -60,13 +61,19 @@ class Moderate():
             #await self.bot.say("Bot does not have permission to delete messages.")
             return
 
-        count = 0
-        async for msg in self.bot.logs_from(ctx.message.channel, limit=(amount*10)):
-                if count == amount:
-                    return
+        delete_list = []
+        async for msg in self.bot.logs_from(ctx.message.channel, limit=(amount*5)):
+                if len(delete_list) == amount:
+                    break
                 if msg.author.id == found.id:
-                    count += 1
-                    await self.bot.delete_message(msg)
+                    delete_list.append(msg)
+
+        if len(delete_list) == 1:
+            await self.bot.delete_message(delete_list[0])
+        elif len(delete_list) >= 2:
+            for i in range(len(delete_list)//100 + 1):
+                await self.bot.delete_messages(delete_list[100*i:100*(i+1)])
+                await asyncio.sleep(0.5)
 
 def setup(bot):
     bot.add_cog(Moderate(bot))
